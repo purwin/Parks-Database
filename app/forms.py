ccc1a6bb7e7987d69b70b2647ed935d3e1c7c6bc
Michael Purwin <mpurwin@gmail.com>
2018-10-21 18:23:09 -0400

    Add exhibition form class

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, FieldList
 from wtforms.validators import DataRequired
 
 
@@ -9,4 +9,29 @@ class form_artist(FlaskForm):
   email = StringField('Email', validators=[DataRequired()])
   phone = StringField('Phone')
   website = StringField('Website')
-  artworks = StringField('Artworks')
+  artworks = FieldList(StringField('Artworks'), min_entries=0)
+
+
+class form_artwork(FlaskForm):
+  pass
+
+
+class form_park(FlaskForm):
+  pass
+
+
+class form_exhibition(FlaskForm):
+  name = StringField('Name')
+  startDate = StringField('Start Date')
+  endDate = StringField('End Date')
+  installStart = StringField('Install Start')
+  installEnd = StringField('Install End')
+  deinstallDate = StringField('De-Install Due Date')
+  prm = StringField('PRM Review')
+
+  # parks = FieldList(StringField('Parks', min_entries=0))
+  # artworks = FieldList(StringField('Artworks', min_entries=0))
+  # organizations = FieldList(StringField('Organizations', min_entries=0))
+
+class form_org(FlaskForm):
+  pass

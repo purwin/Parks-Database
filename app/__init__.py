3265943433ab7dba4ee7de800571d02c3bc3ebbd
Michael Purwin <mpurwin@gmail.com>
2018-11-13 22:15:14 -0500

    Change config to Development mode

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_wtf.csrf import CsrfProtect
+
 
 app = Flask(__name__, instance_relative_config=True)
-app.config.from_object('config')
+
+# Config currently set to Development Mode
+app.config.from_object('config.DevelopmentConfig')
+# app.config.from_object('config')
+
+
+CsrfProtect(app)
 
 db = SQLAlchemy(app)
 

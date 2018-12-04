ee6f15781263eb2263e69d3adc8061737157b9c5
Michael Purwin <mpurwin@gmail.com>
2018-12-03 23:42:53 -0500

    Add model.artwork.children object

--- app/static/js/src/artwork.js
+++ app/static/js/src/artwork.js
@@ -8,7 +8,14 @@ model.artist = new model.Obj("artist");
 model.exhibition = new model.Obj("exhibition");
 
 // Set form POST route based on HTML's action attribute
-model.artwork.post.edit = $('form').attr('action');
+// model.artwork.post.edit = $('form').attr('action');
+
+// Set child datalist classnames
+model.artwork.children = {
+  "artist": ".js-datalist_artist",
+  "exhibition": ".js-datalist_exhibition",
+  "park": ".js-datalist_park"
+};
 
 // Init controller object
 controller.init();

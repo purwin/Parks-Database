3db42107e3ff524e79edfe1d601d94f76b19037c
Michael Purwin <mpurwin@gmail.com>
2018-12-17 10:11:43 -0500

    Read json file

--- app/import_parks.py
+++ app/import_parks.py
@@ -1,13 +1,28 @@
+import json
+
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker
 
 # import db
-from app import db
+# from app import db
 
 # import parks_db
-from parks_db import Park
+# from parks_db import Park
 
-declare parks json file
 
-read json file
+def init_parks(json_file):
+  pass
+  # db.create_all()
+  # read json file
+  with open(json_file, 'r') as r:
+    # Store json object
+    input = json.load(r)
 
-store json object
 
+
+
+if __name__ == '__main__':
+  # Declare parks json file
+  json_file = '/Users/michaelpurwin/Documents/workings/parks database/data/DPR_Parks_001.json'
+
+  init_parks(json_file)

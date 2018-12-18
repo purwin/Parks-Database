58b63f01c02a0b1b0c9c6a575c0987dcc20f27d9
Michael Purwin <mpurwin@gmail.com>
2018-12-18 10:08:01 -0500

    Create determine_borough function

--- import_parks.py
+++ import_parks.py
@@ -2,15 +2,15 @@
 # -*- coding: utf-8 -*-
 
 import json
-
-# from sqlalchemy import create_engine
-# from sqlalchemy.orm import sessionmaker
+import requests
 
 # import current database
 from app import db
 
 # import Park class
 from app.parks_db import Park
+
+
 
 def import_parks(json_file):
   # Start up database
@@ -21,7 +21,6 @@ def import_parks(json_file):
 
 
 def get_parks(json_file):
-  # db.create_all()
   # Read JSON file
   with open(json_file, 'r') as r:
     # Store JSON object
@@ -47,9 +46,7 @@ def get_parks(json_file):
                     'Parkside Playground', 'Fox Playground', 'Rainey Park',
                     'Washington Park']
 
-  # for i in input:
-  #   print i
-  #   return
+  # If park name is a duplicate, add park_id to make unique
   for i in input:
     if i['Name'] in duplicate_list:
       add_park({
@@ -71,21 +68,26 @@ def init_db():
   db.create_all()
 
 
-def add_park(obj):
-  # Determine borough
+def determine_borough(obj):
   if obj['Park_ID'].startswith('X'):
-    borough = 'Bronx'
+    return 'Bronx'
   elif obj['Park_ID'].startswith('B'):
-    borough = 'Brooklyn'
+    return 'Brooklyn'
   elif obj['Park_ID'].startswith('M'):
-    borough = 'Manhattan'
+    return 'Manhattan'
   elif obj['Park_ID'].startswith('Q'):
-    borough = 'Queens'
+    return 'Queens'
   elif obj['Park_ID'].startswith('R'):
-    borough = 'Staten Island'
+    return 'Staten Island'
   else:
     print "ERROR: {}".format(obj)
-    dump_errors(obj)
+    dump_errors("{}: Cannot determine borough.".format(obj['Name']))
+    return None
+
+
+def add_park(obj):
+  # Determine borough
+  borough = determine_borough(obj)
 
   # Determine address
   if obj['Zip']:
@@ -94,21 +96,33 @@ def add_park(obj):
     address = "{}, {} NY".format(obj['Location'], borough)
 
   return
-  new_park = Park(name=obj['Name'],
-                  park_id=obj['Park_ID'],
-                  address=address,
-                  borough=borough)
-  db.session.add(new_park)
-  db.session.commit()
+  try:
+    new_park = Park(name=obj['Name'],
+                    park_id=obj['Park_ID'],
+                    address=address,
+                    borough=borough)
+    db.session.add(new_park)
+    db.session.commit()
+  except Exception as e:
+    dump_errors("{}: {}".format(obj['Name'], e))
 
 
-def dump_errors(obj):
+def get_cb(name):
+  r = requests.get('https://www.nycgovparks.org/parks/{}'.format(name.lower().replace(' ', '-')))
+  if r.status_code == 200:
+    print r.text
+  else:
+    dump_errors("{}: Unable to connect to Parks site".format(name))
+
+
+def dump_errors(error):
   with open('errors_park.txt', 'a') as w:
-    json.dump(obj, w)
+    w.write(error)
 
 
 if __name__ == '__main__':
   # Declare parks JSON file
   json_file = '/Users/michaelpurwin/Documents/workings/parks database/data/DPR_Parks_001.json'
 
-  get_parks(json_file)
+  # get_parks(json_file)
+  get_cb("von briesen park")

6952c534b2ed278477bb38e503fa54eb7898efdc
Michael Purwin <mpurwin@gmail.com>
2019-01-19 23:31:21 -0500

    Create basic import_csv function, object functions

--- app/import.py
+++ app/import.py
@@ -22,22 +22,91 @@ import pandas as pd
 #   db.session.add(park)
 #   db.session.commit()
 
+def import_park(**params):
+  print "PARK!"
+  for key, value in params.items():
+    print "{} equals {}".format(key, value)
 
 
+def import_artist(**params):
+  print "ARTIST!"
+  for key, value in params.items():
+    print "{} equals {}".format(key, value)
+
+
+def import_artwork(**params):
+  print "ARTWORK!"
+  for key, value in params.items():
+    print "{} equals {}".format(key, value)
+
+
+def import_exhibition(**params):
+  print "EXHIBITION!"
+  for key, value in params.items():
+    print "{} equals {}".format(key, value)
+
+
+def import_org(**params):
+  print "ORG!"
+  for key, value in params.items():
+    print "{} equals {}".format(key, value)
+
+
+def object_table(arg):
+  set_obj = {
+    'exhibition': import_exhibition,
+    'artwork': import_artwork,
+    'park': import_park,
+    'artist': import_artist,
+    'org': import_org
+  }
+
+  return set_obj[arg]
+
+
+def import_csv(csv_file, obj, cols, vals):
+  # Get panda dataframe from selected file
+  file = pd.read_csv(csv_file)
+  # Remove empty columns
+  file.drop(file.columns[file.columns.str.contains('unnamed', case=False)],
+    axis=1, inplace=True)
+
+  # TEMP VAR!
+  file = file[1:5]
+
+  # Get Object type, store function value
+  model_object = object_table(obj)
+
+  # Loop through file rows
+  for index, row in file.iterrows():
+    kwargs = {}
+    for col, val in zip(cols, vals):
+      # Store val item as key, value of row item as value
+      kwargs[val] = row[col]
+    # Call relevant function with key/value items
+    model_object(**kwargs)
 
 
 
 def main(csv_file):
-  file = pd.read_csv(csv_file)
-  file.drop(file.columns[file.columns.str.contains('unnamed', case=False)],
-    axis=1, inplace=True)
-  file_headers = file.columns.values
-  row_1 = file[1:5]
-  for index, row in row_1.iterrows():
-    d = {}
-    for col in file_headers:
-      d[col] = row[col]
-    print d
+  temp_obj = 'park'
+  cols = ['Location', 'Borough']
+  vals = ['park_name', 'borough']
+
+  import_csv(csv_file = csv_file, obj = temp_obj, cols = cols, vals = vals)
+
+  # file = pd.read_csv(csv_file)
+  # file.drop(file.columns[file.columns.str.contains('unnamed', case=False)],
+  #   axis=1, inplace=True)
+  # file_headers = file.columns.values
+  # print file_headers
+
+  # row_1 = file[1:5]
+  # for index, row in row_1.iterrows():
+  #   d = {}
+  #   for col in file_headers:
+  #     d[col] = row[col]
+  #   print d
 
 if __name__ == '__main__':
   main('/Users/michaelpurwin/Documents/workings/parks database/data/CURRENT_Public-Art-Chronology_1.30.2017.csv')

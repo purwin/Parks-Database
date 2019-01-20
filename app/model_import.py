a79ea19b9875def76cd44d1b082997b4494718f1
Michael Purwin <mpurwin@gmail.com>
2019-01-20 10:14:18 -0500

    Add artist parameters to import_artist function

--- app/model_import.py
+++ app/model_import.py
@@ -1,5 +1,5 @@
-# from app import app, db
-# from parks_db import Exh_art_park, Exhibition, Park, Artwork, Artist, Org
+from app import app, db
+from parks_db import Exh_art_park, Exhibition, Park, Artwork, Artist, Org
 
 import pandas as pd
 
@@ -30,12 +30,43 @@ def import_park(**params):
 
 def import_artist(**params):
   print "ARTIST!"
-  for key, value in params.items():
-    print "{} equals {}".format(key, value)
+  # Check for existing ID
+  id = params.get('id', None)
+  if id:
+    artist = Artist.query.filter_by(id = id).one()
+  else:
+    artist = Artist()
 
+  try:
+    # Require pName param to add artist to database
+    artist.pName = params['pName']
+    artist.fName = params.get('fName', None)
+    artist.email = params.get('email', None)
+    artist.phone = params.get('phone', None)
+    artist.website = params.get('website', None)
+
+    # for art in params.get('artworks', None):
+    #   artwork = Artwork.query.filter_by(name = art).one()
+    #   artist.artworks.append(artwork)
+
+    db.session.add(artist)
+    db.session.commit()
+  except Exception as e:
+    name = "{} {}".format(params.get('fName'), params.get('pName')) if params.get('fName') else params.get('pName')
+    return "{}: {}".format(name, e)
+
+kwargs = {'a': 'one', 'b': 'two', 'id': 'yoyo', 'pName': 'williams', 'fName': 'doug'}
+kwargs = {'a': 'one', 'b': 'two', 'pName': 'williams', 'fName': 'doug'}
+
+import_artwork(**kwargs)
 
 def import_artwork(**params):
   print "ARTWORK!"
+  # Check for existing ID
+  id = params['id']
+  print id
+  name = "{} {}".format(params.get('fName'), params.get('pName')) if params.get('fName') else params.get('pName')
+  print name
   for key, value in params.items():
     print "{} equals {}".format(key, value)
 

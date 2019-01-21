845cf2059f90ebedd30103192b6897c5f3b59e0e
Michael Purwin <mpurwin@gmail.com>
2019-01-21 18:41:37 -0500

    Reconfigure app/__init__ imports

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,6 @@ from flask_migrate import Migrate
 from flask_wtf.csrf import CsrfProtect
 from flask_talisman import Talisman
 
-from app import views, parks_db, forms, users
-
 
 app = Flask(__name__, instance_relative_config=True)
 
@@ -45,6 +43,7 @@ db = SQLAlchemy(app)
 
 migrate = Migrate(app, db)
 
+from app import views, parks_db, forms, users
 
 if __name__ == '__main__':
   app.run()

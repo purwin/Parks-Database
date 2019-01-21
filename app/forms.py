403532f0c41862454f9e99add34108a9b987f8ba
Michael Purwin <mpurwin@gmail.com>
2019-01-20 23:59:54 -0500

    Replace skip_existing with match_existing

--- app/forms.py
+++ app/forms.py
@@ -161,6 +161,6 @@ class Form_import_data(FlaskForm):
                                  ('artist', 'Artists'),
                                  ('org', 'Orgs')],
                         validators=[DataRequired()])
-  skip_existing = BooleanField('Skip Items Already Added to the Database')
+  match_existing = BooleanField('Match Existing Items in the Database')
   keys = FieldList(FreeSelectField('Key'))
   values = FieldList(FreeSelectField('Value'))

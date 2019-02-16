f89ea02108640a161a637714e567310acee63447
Michael Purwin <mpurwin@gmail.com>
2019-02-16 15:17:07 -0500

    Create get_artist_name function

--- app/add_models/add_artist.py
+++ app/add_models/add_artist.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from app import db
-from app.parks_db import Artist, Artwork
+from app.parks_db import Artist
+import add_artwork
 
 
 # List of acceptable keys for Artist objects
@@ -28,6 +29,17 @@ def add_artist(match=False, **params):
   - "warning": string detailing any unforseen issues
   """
 
+  # If required name parameter not included, return error
+  if not params.get('name'):
+    return {
+      "success": False,
+      "result": "Couldn't determine object name.",
+      "data": params
+    }
+  else:
+    name = params.get('name')
+
+
   print "IMPORT ARTIST!"
   artist = False
   name = "{} {}".format(params.get('fName'), params.get('pName'))\
@@ -38,15 +50,21 @@ def add_artist(match=False, **params):
     artist = Artist.query.filter_by(id=id).first()
   # Or search for existing items if match option is set
   elif match == True:
-    artist = Artist.query.filter_by(pName=params['pName'])\
-                         .filter_by(fName=params['fName']).first()
+    if params.get('pName') and params.get('fName'):
+      artist = Artist.query.filter_by(pName=params['pName'])\
+                           .filter_by(fName=params['fName']).first()
+    elif params.get('name'):
+      artist = Artist.query.filter_by(name=params.get('name')).first()
 
-  action = 'Found {} in the database.\
+  result = 'Found {} in the database.\
             Updated artist with new data.'.format(name)
 
   if not artist:
     artist = Artist()
-    action = 'Added new artist: {}.'.format(name)
+    result = 'Added new artist: {}.'.format(name)
+
+  # Define warnings string to return
+  warnings = ""
 
   # Loop through passed key/value attributes, add to class object
   try:
@@ -54,27 +72,70 @@ def add_artist(match=False, **params):
       if key not in['artworks']:
         setattr(artist, key, value)
 
-    # Loop through artist.artworks separately
-    if 'artworks' in params:
-      print "There's artworks in this!"
-      artworks = params.get('artworks', None)
-      # If artist.artworks is string, convert to list
-      artworks = [artworks] if isinstance(artworks, str) else artworks
-      # Loop through list values if they exist, add to artwork
-      for artwork in artworks or []:
-        art = False
-        # FUTURE: Call artwork function
-        art = Artwork.query.filter_by(name=artwork).first()
-        if not art:
-          art = Artwork(name=artwork)
-          db.session.add(art)
-        if art not in artist.artworks:
-          artist.artworks.append(art)
-
     db.session.add(artist)
+
+
+    # Loop through artist.artworks separately
+    # if 'artworks' in params:
+    #   print "There's artworks in this!"
+    #   artworks = params.get('artworks', None)
+    #   # If artist.artworks is string, convert to list
+    #   artworks = [artworks] if isinstance(artworks, str) else artworks
+    #   # Loop through list values if they exist, add to artwork
+    #   for artwork in artworks or []:
+    #     art = add_artwork.add_artwork(name=artwork)
+
+    #     if art['success'] == True:
+    #       pass
+    #     # art = False
+    #     # FUTURE: Call artwork function
+    #     # art = Artwork.query.filter_by(name=artwork).first()
+    #     # if not art:
+    #       # art = Artwork(name=artwork)
+    #       # db.session.add(art)
+    #     if art not in artist.artworks:
+    #       artist.artworks.append(art)
+
     db.session.commit()
-    print "Success: {}".format(action)
-    return "Success: {}".format(action)
+    db.session.flush()
+
+    # print "Park: {}: {}".format(name, result)
+
+    return {
+      "success": True,
+      "result": result,
+      "warning": warnings,
+      "data": park.serialize
+    }
+
   except Exception as e:
-    print "Error: {}: {}".format(name, e)
-    return "Error: {}: {}".format(name, e)
+    db.session.rollback()
+    print "ERROR! {}".format(e)
+    return {
+      "success": False,
+      "result": "{}: {}".format(name, e),
+      "warning": warnings,
+      "data": params
+    }
+
+
+def get_artist_name(**params):
+  if 'name' not in params:
+    return False
+
+  else:
+    pName = ('pName' in params and params['pName']) or params.get('name').split(' ', 1)[0]
+    fName = ('fName' in params and (params['fName'] and params['pName'])) or params.get('name').split(' ', 1)[1]
+
+  # if params.get('pName'):
+  #   pName = params.get('pName')
+  #   if params.get('fName'):
+  #     fName = params.get('pName')
+  #   else:
+  #     fName = ''
+
+  return {
+     'name': params.get('name'),
+    'fName': fName,
+    'pName': pName
+  }

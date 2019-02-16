f3e8c2b83c4b7ba922d14e8b8a973bd381c02193
Michael Purwin <mpurwin@gmail.com>
2019-02-16 15:27:07 -0500

    Fix module imports, insert add_exh_art_park() ref

--- app/add_models/add_exhibition.py
+++ app/add_models/add_exhibition.py
@@ -1,10 +1,12 @@
+# -*- coding: utf-8 -*-
+
 from datetime import datetime
 
 from app import db
-from app.parks_db import Exhibition, Org, Artwork, Park
-from add_artwork import add_artwork
-from add_park import add_park
-from add_exh_art_park import add_exh_art_park
+from app.parks_db import Exhibition, Org
+import add_artwork
+import add_park
+import add_exh_art_park
 
 
 # List of acceptable keys for Exhibition objects
@@ -55,6 +57,7 @@ def add_exhibition(match=True, **params):
   """
 
   # If required name parameter not included, return error
+
   if not params.get('name'):
     return {
       "success": False,
@@ -71,14 +74,14 @@ def add_exhibition(match=True, **params):
     exhibition = Exhibition.query.filter_by(id=id).first()
   # Or search for existing items if match option is set
   elif match == True:
-    exhibition = Exhibition.query.filter_by(name=params['name']).first()
+    exhibition = Exhibition.query.filter_by(name=name).first()
 
   result = 'Found {} in the database. Updated exhibition with new data.'\
-           .format(params.get('name'))
+           .format(name)
 
   if not exhibition:
     exhibition = Exhibition()
-    result = 'Added new exhibition: {}.'.format(params.get('name'))
+    result = 'Added new exhibition: {}.'.format(name)
 
   # Define warnings string to return
   warnings = ""
@@ -88,7 +91,7 @@ def add_exhibition(match=True, **params):
     for key, value in params.iteritems():
       # Check for bad keys in object
       if key not in exhibition_params:
-        return "Error: {}: Unexpected {} attribute found.".format(params.get('name'), key)
+        return "Error: {}: Unexpected {} attribute found.".format(name, key)
       # Add non-arry key items to exhibition object
       if key not in ['exh_art_park', 'orgs', 'artworks', 'parks']:
         setattr(exhibition, key, value)
@@ -103,7 +106,7 @@ def add_exhibition(match=True, **params):
 
     # Add any orgs to exhibitions.org
     if 'orgs' in params:
-      print "There's orgs in {}!".format(params.get('name'))
+      print "There's orgs in {}!".format(name)
       orgs = params.get('orgs', None)
       # If exhibition.orgs is string, convert to list
       orgs = [orgs] if isinstance(orgs, str) else orgs
@@ -119,55 +122,59 @@ def add_exhibition(match=True, **params):
         if organization not in exhibition.orgs:
           exhibition.orgs.append(organization)
 
-    # Add exh_art_park relationships
-    if 'artworks' and 'parks' in params:
-      # Flush session to get and use exhibition ID
-      db.session.flush()
+    # # Add exh_art_park relationships
+    # if 'artworks' and 'parks' in params:
+    #   # Flush session to get and use exhibition ID
+    #   db.session.flush()
 
-      print "There's artworks and parks in {}!".format(name)
-      artworks = filter(None, params.get('artworks', None))
-      # If exhibition.artworks is string, convert to list
-      artworks = [artworks] if isinstance(artworks, str)\
-                                  else artworks
+    #   print "There's artworks and parks in {}!".format(name)
+    #   artworks = filter(None, params.get('artworks', None))
+    #   # If exhibition.artworks is string, convert to list
+    #   artworks = [artworks] if isinstance(artworks, str)\
+    #                               else artworks
 
-      parks = filter(None, params.get('parks', None))
-      # If exhibition.parks is string, convert to list
-      parks = [parks] if isinstance(parks, str) else parks
+    #   parks = filter(None, params.get('parks', None))
+    #   # If exhibition.parks is string, convert to list
+    #   parks = [parks] if isinstance(parks, str) else parks
 
-      if len(artworks) != len(parks):
-        warnings += 'There’s an uneven number of artworks and parks in '\
-                    '{}. Skipping addition.\n'.format(name)
-      else:
+    #   if len(artworks) != len(parks):
+    #     warnings += 'There’s an uneven number of artworks and parks in '\
+    #                 '{}. Skipping addition.\n'.format(name)
+    #   else:
 
-        for artwork, park in zip(artworks, parks):
-          park_dict = add_park({name: park})
-          print "Park dict: {}".format(park_dict)
-          park_id = park_dict.data.id
-          print "Park ID: {}".format(park_id)
+    #     for artwork, park in zip(artworks, parks):
+    #       park_dict = add_park.add_park({name: park})
+    #       print "Park dict: {}".format(park_dict)
+    #       park_id = park_dict.data.id
+    #       print "Park ID: {}".format(park_id)
 
-          artwork_dict = add_artwork({name: artwork})
-          print "Artwork dict: {}".format(artwork_dict)
-          artwork_id = artwork_dict.data.id
-          print "Artwork ID: {}".format(artwork_id)
+    #       artwork_dict = add_artwork.add_artwork({name: artwork})
+    #       print "Artwork dict: {}".format(artwork_dict)
+    #       artwork_id = artwork_dict.data.id
+    #       print "Artwork ID: {}".format(artwork_id)
 
-          exh_art_park = add_exh_art_park(
-              artwork_id=artwork_id,
-                    park_id=park_id,
-                 exhibition_id=exhibition.id)
+    #       exh_art_park = add_exh_art_park.add_exh_art_park(
+    #           artwork_id=artwork_id,
+    #                 park_id=park_id,
+    #              exhibition_id=exhibition.id)
 
-          if exh_art_park.success == True:
-            result += "\nAdded {} @ {} to the {} artwork"\
-                      .format(artwork, park, exhibition.name)
-          else:
-            warnings += "\n{}".format(exh_art_park.result)
+    #       if exh_art_park.success == True:
+    #         result += "\nAdded {} @ {} to the {} artwork"\
+    #                   .format(artwork, park, exhibition.name)
+    #       else:
+    #         warnings += "\n{}".format(exh_art_park.result)
 
-      db.session.commit()
-      return {
-        "success": True,
-        "result": result,
-        "warning": warnings,
-        "data": exhibition.serialize
-      }
+    db.session.commit()
+    db.session.flush()
+
+    # print "Exhibition: {}: {}".format(name, result)
+
+    return {
+      "success": True,
+      "result": result,
+      "warning": warnings,
+      "data": exhibition.serialize
+    }
 
   except Exception as e:
     db.session.rollback()

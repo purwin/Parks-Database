0a29d5bf4d49a7c9cd1f0e55923f1ec718645471
Michael Purwin <mpurwin@gmail.com>
2019-02-25 20:39:37 -0500

    Init saved webpack config

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const devMode = process.env.NODE_ENV !== 'production'
+
+module.exports = {
+  watch: true,
+  entry: {
+    park: './app/static/js/src/park.js',
+    exhibition: './app/static/js/src/exhibition.js',
+    org: './app/static/js/src/org.js',
+    artwork: './app/static/js/src/artwork.js',
+    artist: './app/static/js/src/artist.js',
+    create: './app/static/js/src/create.js',
+    import: './app/static/js/src/import.js',
+    style: './app/static/style/src/style.scss'
+  },
+  output: {
+    path: path.resolve(__dirname, './app/static/js/dist'),
+    filename: '[name].bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+      {
+        test: /\.scss$/,
+        use: [
+            // fallback to style-loader in development
+            devMode !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader,
+            "css-loader",
+            "sass-loader"
+        ]
+    }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: "[name].bund.css"
+    })
+  ],
+  stats: {
+    colors: true
+  },
+  devtool: 'source-map',
+  optimization: {
+      splitChunks: {
+        cacheGroups: {
+          commons: {
+            name: 'commons',
+            chunks: 'initial',
+            minChunks: 2
+          }
+        }
+      }
+    }
+};

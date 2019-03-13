d0e40303020a53391c5c3603d0509e574186e87d
Michael Purwin <mpurwin@gmail.com>
2019-03-12 20:51:03 -0400

    Add comments to $().DataTable()

--- app/static/js/src/exhibition.js
+++ app/static/js/src/exhibition.js
@@ -34,7 +34,7 @@ model.exhibition.children = {
 // Init controller object
 controller.init();
 
-
+// Call MDBootstrap function to set table as sortable
 $('#js-table_artwork').DataTable({
   // Sort table based on initial column (Artworks)
   "order": [[ 0, "asc" ]],

14db375dc077f2a6ad514e81cd1dfc3381d5116a
Michael Purwin <mpurwin@gmail.com>
2019-03-17 10:40:52 -0400

    Add test for duplicate usernname signup

--- tests/test_users.py
+++ tests/test_users.py
@@ -5,10 +5,16 @@ from flask import request, session
 
 from base import BaseTests
 
+from app import db
 from app.users import User
 
 
 class TestUsers(BaseTests):
+
+  default_user = {
+    "username": 'cool_username',
+    "password": 'party123456'
+  }
 
   # Test signup success
   def test_valid_admin_login(self):
@@ -26,10 +32,29 @@ class TestUsers(BaseTests):
 
   # Test signup success
   def test_valid_user_signup(self):
-    signup_user = {
-      "username": 'cool_username',
-      "password": 'party123456'
-    }
+    with self.app as c:
+      with c.session_transaction() as sess:
+        sess['url'] = '/'
+
+      self.login()
+
+      response = self.signup(
+          username=self.default_user['username'],
+          password=self.default_user['password']
+      )
+
+      user = current_user.username
+
+    self.assertEqual(response.status_code, 200)
+    self.assertEqual(user, self.default_user['username'])
+
+
+  # Test signup duplicate
+  def test_invalid_user_signup_duplicate(self):
+    db.session.add(User(
+        username=self.default_user['username'],
+        password=self.default_user['password']))
+    db.session.commit()
 
     with self.app as c:
       with c.session_transaction() as sess:
@@ -38,17 +63,14 @@ class TestUsers(BaseTests):
       self.login()
 
       response = self.signup(
-          username=signup_user['username'],
-          password=signup_user['password']
+          username=self.default_user['username'],
+          password='party123456'
       )
 
-      user = current_user.username
+      req = request.url
 
-    self.assertEqual(response.status_code, 200)
-    self.assertEqual(user, signup_user['username'])
-
-
-
+    self.assertIn(b'Please use a different username.', response.data)
+    self.assertIn(b'/signup', req)
 
 
 if __name__ == "__main__":

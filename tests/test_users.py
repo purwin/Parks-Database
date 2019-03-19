5b9c26c1eaa1e51602270319d974e28917aef26b
Michael Purwin <mpurwin@gmail.com>
2019-03-18 20:04:31 -0400

    Add tests for wrong password, wrong user, logout

--- tests/test_users.py
+++ tests/test_users.py
@@ -79,19 +79,65 @@ class TestUsers(BaseTests):
       with c.session_transaction() as sess:
         sess['url'] = '/'
 
-      response = self.signup(
+      self.signup(
           username=self.default_user['username'],
           password='party123456'
       )
 
       req = request.url
 
-    print "test_invalid_user_signup_not_admin: ", response
-
     user = User.query.filter_by(username=self.default_user['username']).first()
 
     self.assertIn(b'/login', req)
     self.assertIsNone(user)
+
+
+  # Test login wrong password
+  def test_invalid_user_login_password(self):
+    with self.app as c:
+      with c.session_transaction() as sess:
+        sess['url'] = '/'
+
+      response = self.login(username="admin", password="poolparty4eva")
+
+      req = request.url
+
+    self.assertIn(b'Incorrect password!', response.data)
+    self.assertIn(b'/login', req)
+
+
+  # Test login wrong user
+  def test_invalid_user_login_no_user(self):
+    with self.app as c:
+      with c.session_transaction() as sess:
+        sess['url'] = '/'
+
+      response = self.login(username="not_admin", password="poolparty4eva")
+
+      req = request.url
+
+    self.assertIn(b'User not found!', response.data)
+    self.assertIn(b'/login', req)
+
+
+  # Test logout
+  def test_valid_user_logout(self):
+    with self.app as c:
+      with c.session_transaction() as sess:
+        sess['url'] = '/'
+
+      self.login()
+
+      response = self.logout()
+
+      req = request.url
+
+    self.assertEqual(response.status_code, 200)
+    self.assertIn(b'/login', req)
+
+
+  # Test logout not signed in
+  # FUTURE: Views: Update to redirect to home()
 
 
 if __name__ == "__main__":

8a8c6e22b7168d7da5ce8c247ccc3e1f351a34a1
Michael Purwin <mpurwin@gmail.com>
2019-03-25 12:03:16 -0400

    Add staticmethod create_park

--- tests/test_routes_park.py
+++ tests/test_routes_park.py
@@ -8,6 +8,31 @@ from base import BaseTests
 
 class TestRoutesPark(BaseTests):
 
+  default_park = dict(
+      name='NY Park',
+      park_id='W450',
+      borough='Queens',
+      address='30 Broadway',
+      cb='04'
+  )
+
+
+  @staticmethod
+  def create_park(**kwargs):
+    """
+    Static method to add park class object to database
+    Takes the following string args: name, park_id, borough, address, cb
+    Adds class to Park database, commits session, and flushes to get id val
+    Returns the created class instance
+    """
+    park = Park(**kwargs)
+    db.session.add(park)
+    db.session.commit()
+    db.session.flush()
+
+    return park
+
+
   # Test parks page logged in
   def test_valid_parks_logged_in(self):
     with self.app as c:
@@ -74,15 +99,13 @@ class TestRoutesPark(BaseTests):
   # Test park page not logged in
   def test_invalid_park_not_logged_in(self):
     # Add park to database
-    park = Park(
-        name='NY Park',
-        park_id='W450',
-        borough='Queens',
-        address='30 Broadway',
-        cb='04'
+    self.create_park(
+        name=self.default_park['name'],
+        park_id=self.default_park['park_id'],
+        borough=self.default_park['borough'],
+        address=self.default_park['address'],
+        cb=self.default_park['cb']
     )
-    db.session.add(park)
-    db.session.commit()
 
     with self.app:
       response = self.app.get('/parks/1', follow_redirects=True)

ac051a9f1f76a6d4695d63fd50ed850c27510757
Michael Purwin <mpurwin@gmail.com>
2019-09-26 21:04:03 -0400

    Add test_artwork_create functions

--- tests/test_routes_artwork.py
+++ tests/test_routes_artwork.py
@@ -53,19 +53,92 @@ class TestRoutesArtwork(BaseTests):
     self.assertEqual(response.status_code, 200)
 
 
-  # Test artworks page not logged in
-
-  # Test artworks page logged in
-
-
   # Test artwork page not logged in
+  def test_invalid_artwork_not_logged_in(self):
+    artwork = self.default_artwork
+    # Add artwork to database
+    self.create_artwork(**artwork)
+
+    with self.app:
+      response = self.app.get('/artworks/1', follow_redirects=True)
+      req = request.url
+
+    self.assertIn(b'/login', req)
+    self.assertEqual(response.status_code, 200)
+
 
   # Test artwork page logged in
+  def test_valid_artwork_logged_in(self):
+    artwork = self.default_artwork
+    # Add artwork to database
+    self.create_artwork(**artwork)
+
+    with self.app as c:
+      with c.session_transaction() as sess:
+        sess['url'] = '/'
+
+      self.login()
+      response = self.app.get('/artworks/1', follow_redirects=True)
+      req = request.url
+
+    self.assertIn(b'/artworks/1', req)
+    self.assertEqual(response.status_code, 200)
+
+
+ # Test artwork page with no artworks
+  def test_invalid_artwork_no_artworks(self):
+    with self.app as c:
+      with c.session_transaction() as sess:
+        sess['url'] = '/'
+
+      self.login()
+      response = self.app.get('/artworks/1', follow_redirects=True)
+      req = request.url
+
+    self.assertIn(b'/artworks/1', req)
+    self.assertEqual(response.status_code, 404)
+
+
+  # Test GET artwork CREATE page
+  def test_invalid_artwork_create_get(self):
+    with self.app:
+      response = self.app.get('/artworks/create', follow_redirects=True)
+
+    self.assertIn('Method Not Allowed', response.data)
+    self.assertEqual(response.status_code, 405)
+
+
+  # Test artwork CREATE page logged in
+  def test_valid_artwork_create_post(self):
+    artwork = self.default_artwork
+    with self.app as c:
+      with c.session_transaction() as sess:
+        sess['url'] = '/'
+
+      self.login()
+      response = self.app.post(
+          '/artworks/create',
+          data=artwork,
+          follow_redirects=True
+      )
+
+    self.assertIn('"success": true', response.data)
+    self.assertEqual(response.status_code, 200)
 
 
   # Test artwork CREATE page not logged in
+  def test_invalid_artwork_create_post(self):
+    artwork = self.default_artwork
+    with self.app as c:
+      response = self.app.post(
+          '/artworks/create',
+          data=artwork,
+          follow_redirects=True
+      )
+      req = request.url
 
-  # Test artwork CREATE page logged in
+    self.assertIn(b'/login', req)
+    self.assertEqual(response.status_code, 200)
 
 
   # Test artwork EDIT page not logged in

928fde4f16f7c503d3b81462e64607265e07a19a
Michael Purwin <mpurwin@gmail.com>
2019-09-26 22:05:56 -0400

    Add test_artist_create functions

--- tests/test_routes_artist.py
+++ tests/test_routes_artist.py
@@ -103,9 +103,46 @@ class TestRoutesArtist(BaseTests):
     self.assertEqual(response.status_code, 404)
 
 
-  # Test artist CREATE page not logged in
+  # Test GET artist CREATE page
+  def test_invalid_artist_create_get(self):
+    with self.app:
+      response = self.app.get('/artists/create', follow_redirects=True)
+
+    self.assertIn('Method Not Allowed', response.data)
+    self.assertEqual(response.status_code, 405)
+
 
   # Test artist CREATE page logged in
+  def test_valid_artist_create_post(self):
+    artist = self.default_artist
+    with self.app as c:
+      with c.session_transaction() as sess:
+        sess['url'] = '/'
+
+      self.login()
+      response = self.app.post(
+          '/artists/create',
+          data=artist,
+          follow_redirects=True
+      )
+
+    self.assertIn('"success": true', response.data)
+    self.assertEqual(response.status_code, 200)
+
+
+  # Test artist CREATE page not logged in
+  def test_invalid_artist_create_post(self):
+    artist = self.default_artist
+    with self.app as c:
+      response = self.app.post(
+          '/artists/create',
+          data=artist,
+          follow_redirects=True
+      )
+      req = request.url
+
+    self.assertIn(b'/login', req)
+    self.assertEqual(response.status_code, 200)
 
 
   # Test artist EDIT page not logged in
